a80c41b0a3615ec81063b041895546c742b9f30b
sultanhxsn <sultanhasanminai@gmail.com>
2025-01-12 23:19:08 -0800

    recent push

--- my-react-lab/src/App.js
+++ my-react-lab/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import './Styles/App.css'; 
-import Card from './Components/Card.js';
+import React from "react";
+import Navbar from "./Components/Navbar";
+import LandingPage from "./Components/LandingPage";
+import SpecialtyMenu from "./Components/SpecialtyMenu";
+import Footer from "./Components/Footer";
+import Counter from "./Components/Counter";
+import InputName from "./Components/Calculator";
+import NameAge from "./Components/NameAge";
+import QuizApp from "./Components/QuizApp";
+import Calculator from "./Components/Calculator";
+import LoginForm from "./Components/LoginForm";
+import Counter1 from './Components/Counter1';
+import Calc1 from "./Components/Calc1";
 
 function App() {
+  const menuItems = [
+    { title: "Spicy Chicken Wings", description: "Our signature spicy wings", price: 500 },
+    { title: "Classic Fried Chicken", description: "Traditional recipe", price: 350 },
+    { title: "Spicy Chicken Wings", description: "Our signature spicy wings", price: 650 },
+  ];
+
   return (
     <div>
-      <Card name="Nainitalwala" age={40} occupation="Parchoon Ki Dukan" location="Dhoraji" image="image.jpg"/>
+      <Counter1 />
+      <Calc1 />
     </div>
   );
 }
